a4ca42577c395fea0aa8ae2c0e32a688e7fd3906
Ananye Agarwal <ananayagarwal@gmail.com>
2019-11-04 06:46:13 +0530

    Added the vector space of univariate polynomials

---
+++ poly_univar.py
@@ -0,0 +1,118 @@
+# This module implements the vector of homogenous bivariate polynomial 
+# The coefficients can be derived from any scalar class
+
+from simple_vectors import Vector
+from utils import matrix_rank
+
+class PolyUnivar(Vector):
+    def __init__(self, arr):
+        # Initialize the polynomial with coefficients
+        if (len(arr) == 0):
+            self.coeffs = []
+            self.deg = 0
+        elif (arr[0] == arr[0].zero()):
+            raise Exception("The first coefficient of a polynomial cannot be zero")
+        else:
+            self.coeffs = arr
+            self.deg = len(arr)
+
+    def __repr__(self):
+        return "(" + repr(self.coeffs) + ", " + repr(self.deg) + ")"
+
+    def __add__(self, other):
+        # If the degree of the other is more, swap the two variables
+        if (self.deg < other.deg):
+            self, other = other, self
+
+        offset = self.deg - other.deg
+
+        if (self.deg == 0):
+            return PolyUnivar([])
+        
+        # Copy the first self.deg - other.deg coeffs
+        result = [self.coeffs[i] for i in range(offset)]
+    
+        # Add the remaining values
+        for i in range(other.deg):
+            result.append(other.coeffs[i] + self.coeffs[i + offset])
+
+        # Remove leading zeros 
+        zeroElement = self.coeffs[0].zero()
+        ctr = 0
+        maxctr = len(result)
+        while(ctr < maxctr):
+            if (result[ctr] == zeroElement):
+                ctr += 1
+            else:
+                break 
+        result = result[ctr:]
+
+        return PolyUnivar(result)
+
+    def __mul__(self, other):
+        if (self.deg < other.deg):
+            self, other = other, self 
+
+        if (other.deg == 0):
+            if (self.deg == 0):
+                return 0
+            else:
+                return self.coeffs[0].zero()
+
+        result = self.coeffs[0].zero()
+        offset = self.deg - other.deg
+        
+        for i in range(other.deg):
+            result = result + self.coeffs[i + offset] * other.coeffs[i]
+        
+        return result 
+
+    def __rmul__(self, other):
+        if (other == other.zero()):
+            return PolyUnivar([])
+
+        result = []
+
+        for i in range(self.deg):
+            result.append(other * self.coeffs[i])
+        
+        return PolyUnivar(result)
+
+    def __eq__(self, other):
+        return self.coeffs == other.coeffs 
+
+    def zero(self):
+        return PolyUnivar([])
+
+    def linearlyIndependent(self, arr):
+        m = len(arr)
+
+        # The empty set is linearly independent
+        if (m == 0):
+            return True
+
+        # Find the maximal degree and a zero element 
+        maxDegree = 0
+        zeroElement = None
+
+        for p in arr:
+            maxDegree = max(maxDegree, p.deg)
+            if (p.deg > 0 and zeroElement == None):
+                zeroElement = p.coeffs[0].zero()
+        
+        if maxDegree == 0:
+            return False
+
+        M = [[zeroElement for i in range(maxDegree)] for j in range(m)]
+
+        for i in range(m):
+            offset = maxDegree - arr[i].deg 
+            for j in range(offset):
+                M[i][j] = zeroElement
+            
+            for j in range(arr[i].deg):
+                M[i][j + offset] = arr[i].coeffs[j]
+
+        return matrix_rank(M) == m
+
+
---
+++ test_poly_univar.py
@@ -0,0 +1,99 @@
+# Test the hom poly bivar module over R and Zm
+
+from real import R 
+from mod_integer import Zm 
+from poly_univar import PolyUnivar
+import unittest
+
+class TestPolyUnivarReal(unittest.TestCase):
+    def test_eq(self):
+        a = PolyUnivar([R(1), R(2), R(3)])
+        b = PolyUnivar([R(1), R(2), R(3)])
+        c = PolyUnivar([R(1), R(1), R(2), R(3)])
+        self.assertEqual(a, b)
+        self.assertNotEqual(a, c)
+
+    def test_add(self):
+        a = PolyUnivar([R(1), R(2), R(3)])
+        b = PolyUnivar([R(-1), R(2), R(3)])
+        c = PolyUnivar([R(4), R(6)])
+        d = PolyUnivar([R(4), R(6), R(6)])
+        self.assertEqual(c, a + b)
+        self.assertNotEqual(d, a + b)
+
+    def test_mul(self):
+        a = PolyUnivar([R(1), R(2), R(3)])
+        b = PolyUnivar([R(1), R(2), R(3), R(4)])
+        self.assertEqual(R(20), a * b)
+        self.assertNotEqual(R(21), a * b)
+
+    def test_rmul(self):
+        a = PolyUnivar([R(1), R(2), R(3)])
+        b = PolyUnivar([R(2), R(4), R(6)])
+        c = PolyUnivar([R(2), R(4), R(7)])
+        self.assertEqual(b, R(2) * a)
+        self.assertEqual(PolyUnivar([]), R(0) * a)
+        self.assertNotEqual(c, R(2) * a)
+        self.assertNotEqual(c, R(0) * a)
+
+    def test_zero(self):
+        a = PolyUnivar([R(1), R(2), R(3)])
+        self.assertEqual(a.zero() + a, a)
+        self.assertEqual(a.zero(), R(0) * a)
+
+    def test_linearlyIndependent(self):
+        a = PolyUnivar([R(1), R(2), R(3)])
+        b = PolyUnivar([R(2), R(3), R(4)])
+        c = PolyUnivar([R(3), R(4), R(5)])
+        d = PolyUnivar([R(3), R(5), R(5)])
+        self.assertTrue(a.linearlyIndependent([a, b, d]))
+        self.assertFalse(a.linearlyIndependent([a, b, c]))
+        self.assertFalse(a.linearlyIndependent([a, b, PolyUnivar([])]))
+
+class TestPolyUnivarModInteger(unittest.TestCase):
+    def test_eq(self):
+        a = PolyUnivar([Zm(1, 31), Zm(2, 31), Zm(3, 31)])
+        b = PolyUnivar([Zm(1, 31), Zm(2, 31), Zm(34, 31)])
+        c = PolyUnivar([Zm(1, 31), Zm(1, 31), Zm(2, 31), Zm(3, 31)])
+        self.assertEqual(a, b)
+        self.assertNotEqual(a, c)
+
+    def test_add(self):
+        a = PolyUnivar([Zm(1, 31), Zm(2, 31), Zm(3, 31)])
+        b = PolyUnivar([Zm(30, 31), Zm(29, 31), Zm(28, 31)])
+        d = PolyUnivar([Zm(4, 31), Zm(6, 31), Zm(6, 31)])
+        self.assertEqual(a.zero(), a + b)
+        self.assertNotEqual(d, a + b)
+
+    def test_mul(self):
+        a = PolyUnivar([Zm(1, 7), Zm(2, 7), Zm(3, 7)])
+        b = PolyUnivar([Zm(1, 7), Zm(2, 7), Zm(3, 7), Zm(4, 7)])
+        self.assertEqual(Zm(6, 7), a * b)
+        self.assertNotEqual(Zm(0, 7), a * b)
+
+    def test_rmul(self):
+        a = PolyUnivar([Zm(1, 7), Zm(2, 7), Zm(3, 7)])
+        b = PolyUnivar([Zm(2, 7), Zm(4, 7), Zm(-1, 7)])
+        c = PolyUnivar([Zm(2, 7), Zm(4, 7), Zm(0, 7)])
+        self.assertEqual(b, Zm(2, 7) * a)
+        self.assertEqual(PolyUnivar([]), Zm(0, 7) * a)
+        self.assertNotEqual(c, Zm(2, 7) * a)
+        self.assertNotEqual(c, Zm(0, 7) * a)
+
+    def test_zero(self):
+        a = PolyUnivar([Zm(1, 7), Zm(2, 7), Zm(3, 7)])
+        self.assertEqual(a.zero() + a, a)
+        self.assertEqual(a.zero(), Zm(0, 7) * a)
+
+    def test_linearlyIndependent(self):
+        a = PolyUnivar([Zm(1, 5), Zm(2, 5), Zm(3, 5)])
+        b = PolyUnivar([Zm(2, 5), Zm(3, 5), Zm(4, 5)])
+        c = PolyUnivar([Zm(3, 5), Zm(4, 5), Zm(0, 5)])
+        d = PolyUnivar([Zm(3, 5), Zm(0, 5), Zm(0, 5)])
+        self.assertTrue(a.linearlyIndependent([a, b, d]))
+        self.assertFalse(a.linearlyIndependent([a, b, c]))
+        self.assertFalse(a.linearlyIndependent([a, b, PolyUnivar([])]))
+
+
+if __name__ == "__main__":
+    unittest.main()
